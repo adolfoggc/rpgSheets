716d3bcf71438096af5170f5a1e3c254ca151743
Carlos Adolfo <adolfo.ggc@gmail.com>
2017-10-15 11:42:49 -0300

    alteration in routes and view of Rpg Systems

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :rpg_systems
+  resources :rpg_systems, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 root 'welcome#index'
