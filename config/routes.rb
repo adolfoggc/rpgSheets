8250a93010d29339933de8d5ceddc79d9db1d399
Carlos Adolfo <adolfo.ggc@gmail.com>
2017-10-15 12:53:55 -0300

    route alteration

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :rpg_systems, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-root 'char3p5s#index'
+root 'char3p5_sheets#index'
 
 
 
