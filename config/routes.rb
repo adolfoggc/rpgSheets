b4b94d009a2bf4eb44588f0bb18f89157dbcf36f
Carlos Adolfo <adolfo.ggc@gmail.com>
2017-10-15 12:19:38 -0300

    new root page

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :rpg_systems, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-root 'welcome#index'
+root 'char3p5s#index'
 
 
 
